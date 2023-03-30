0520b1caff316c5ff60ef3428b63a9a38b0ed1b8
jomg-github <hodduco_o@naver.com>
2023-03-30 23:03:23 +0900

    #06-1 스프링 DB 접근 기술 - 순수 JDBC

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,60 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository repository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("조민기");
+
+        // when
+        Long resultId = memberService.join(member);
+
+        // then
+        assertThat(member.getName()).isEqualTo(memberService.findOne(resultId).get().getName());
+    }
+
+    @Test
+    void 회원가입_X_중복회원() {
+        // given
+        Member member = new Member();
+        member.setName("조민기");
+
+        Member member2 = new Member();
+        member2.setName("조민기");
+        // when
+        memberService.join(member);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
